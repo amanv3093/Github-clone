e26993f95db29951a2504c0a8be151590cf3e036
Md Ismail <mdismaeelkhan345@gmail.com>
2023-12-23 12:33:27 +0530

    clic-event Added as well linked all pages

---
+++ codespace/Foam-Template-page/scripts.js
@@ -0,0 +1,19 @@
+// let l=document.querySelector();
+let l=document.getElementById("event-12");
+
+function fg(){
+   console.log("hjdfhdg")
+   if(l.style.display==="block"){
+    l.style.display="none"
+
+    
+   }else{
+    l.style.display="block"
+   }
+}
+
+    let um = document.getElementById("xyz");
+        um.addEventListener("click", ()=>{
+            l.style.display="none"
+            console.log("abcfd")
+        });
